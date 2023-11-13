a7a00f04e1e3c2e1d852cf823c35db7c2ed4c3d9
Davide Tonelli <davtonell@gmail.com>
2023-11-13 22:49:14 +0100

    Aggiunto CMake per la compilazione

--- src/server.c
+++ src/server.c
@@ -6,7 +6,8 @@
 #include <sys/select.h>
 #include <unistd.h> 
 #include <stdlib.h>
-#include "include/constants.h"
+#include "wrapFuncs/wrapFunc.h"
+#include "constants.h"
 #include <semaphore.h>
 #include <sys/mman.h>
 
