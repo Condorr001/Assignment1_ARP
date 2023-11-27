1e26c9cb3339d0ebec02b5a8e4c27137e90596d7
Davide Tonelli <davtonell@gmail.com>
2023-11-27 23:41:38 +0100

    added logs for velocity and force

--- src/server.c
+++ src/server.c
@@ -38,13 +38,6 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    struct pos drone_current_pos;
-
-    // logfile
-    char filename_string[80];
-    sprintf(filename_string, "log/log.log");
-    FILE *F0;
-
     int pid = Fork();
 
     if (pid) {
@@ -72,8 +65,16 @@ int main(int argc, char *argv[]) {
                              MAP_SHARED, shm, 0);
         memset(shm_ptr, 0, MAX_SHM_SIZE);
 
-        // every two seconds write in the logfile the state of the drone (it
-        // would be better to write only if the status has changed)
+        struct pos drone_current_pos;
+        struct velocity drone_current_velocity;
+        struct force drone_current_force;
+
+        // logfile
+        char filename_string[80];
+        sprintf(filename_string, "log/log.log");
+        FILE *F0;
+
+        // every two seconds write in the logfile the state of the drone
         while (1) {
             Sem_wait(sem_force);
             Sem_wait(sem_velocity);
@@ -81,11 +82,27 @@ int main(int argc, char *argv[]) {
 
             sscanf(shm_ptr + SHM_OFFSET_POSITION, "%f|%f", &drone_current_pos.x,
                    &drone_current_pos.y);
+            sscanf(shm_ptr + SHM_OFFSET_VELOCITY_COMPONENTS, "%f|%f",
+                   &drone_current_velocity.x_component,
+                   &drone_current_velocity.y_component);
+            sscanf(shm_ptr + SHM_OFFSET_FORCE_COMPONENTS, "%f|%f",
+                   &drone_current_force.x_component,
+                   &drone_current_force.y_component);
             // write in the logfile
             F0 = Fopen(filename_string, "a");
-            fprintf(F0, "[INFO] - The x-y position of the drone is: %f %f\n",
-                    drone_current_pos.x, drone_current_pos.y);
+            fprintf(
+                F0,
+                "[INFO] - The x-y position of the drone is: %f %f\n"
+                "[INFO] - The x-y components of the drone velocity are: %f %f\n"
+                "[INFO] - The x-y components of the drone force are: %f %f\n",
+                drone_current_pos.x, drone_current_pos.y,
+                drone_current_velocity.x_component,
+                drone_current_velocity.y_component,
+                drone_current_force.x_component,
+                drone_current_force.y_component);
+
             fclose(F0);
+
             Sem_post(sem_position);
             Sem_post(sem_velocity);
             Sem_post(sem_force);
